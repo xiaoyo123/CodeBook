1aa30207da74717ac1333253b9b24a808d548f4d
xiaoyo123 <36474975azl@gmail.com>
2022-06-06 17:02:24 +0800

    treap modify

--- Treap/default.cpp
+++ Treap/default.cpp
@@ -1,5 +1,5 @@
 struct treap{
-    int key, pri, sz;
+    int key, pri, sz, tag;
     treap *l, *r;
     treap(){};
     treap(int _key){
@@ -18,22 +18,36 @@ int Size(treap *x){
 void pull(treap *x){
     x->sz = Size(x->l) + Size(x->r) + 1;
 }
+void push(treap *x){
+    if(x->tag){
+        swap(x->l, x->r);
+        if(x->l) x->l->tag ^= 1;
+        if(x->r) x->r->tag ^= 1;
+        x->tag = 0;
+    }
+}
 treap* merge(treap *a, treap *b){
     if(!a || !b) return a ? a : b;
     if(a->pri > b->pri){
+        // push(a);
         a->r = merge(a->r, b);
         pull(a);
         return a;
     }
     else{
+        // push(b)
         b->l = merge(a, b->l);
         pull(b);
         return b;
     }
 }
 void splitByKey(treap *x, int k, treap *&a, treap *&b){
-    if(!x) a = b = nullptr;
-    else if(x->key <= k){
+    if(!x){
+        a = b = nullptr;
+        return;
+    }
+    // push(x)
+    if(x->key <= k){
         a = x;
         splitByKey(x->r, k, a->r, b);
         pull(a);
@@ -45,8 +59,12 @@ void splitByKey(treap *x, int k, treap *&a, treap *&b){
     }
 }
 void splitByKth(treap *x, int k, treap *&a, treap *&b){
-    if(!x) a = b = nullptr;
-    else if(Size(x->l) + 1 <= k){
+    if(!x){
+        a = b = nullptr;
+        return;
+    }
+    // push(x);
+    if(Size(x->l) + 1 <= k){
         a = x;
         splitByKth(x->r, k - Size(x->l) - 1, a->r, b);
         pull(a);
